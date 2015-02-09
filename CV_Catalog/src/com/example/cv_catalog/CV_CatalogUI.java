73f6f7d00dc495c92617d383c3cf840abcc07f90
Mikeux <mikeux@gmail.com>
2015-02-09 20:10:30 +0100

    Begining

---
+++ CV_Catalog/src/com/example/cv_catalog/CV_CatalogUI.java
@@ -0,0 +1,40 @@
+package com.example.cv_catalog;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@SuppressWarnings("serial")
+@Theme("cv_catalog")
+public class CV_CatalogUI extends UI {
+
+	@WebServlet(value = "/*", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = CV_CatalogUI.class)
+	public static class Servlet extends VaadinServlet {
+	}
+
+	@Override
+	protected void init(VaadinRequest request) {
+		final VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(true);
+		setContent(layout);
+
+		Button button = new Button("Click Me");
+		button.addClickListener(new Button.ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				u.uzen("dsfdsff");
+				layout.addComponent(new Label("Thank you for clicking"));
+			}
+		});
+		layout.addComponent(button);
+	}
+
+}
---
+++ CV_Catalog/src/com/example/cv_catalog/u.java
@@ -0,0 +1,21 @@
+package com.example.cv_catalog;
+
+import com.vaadin.server.Page;
+import com.vaadin.shared.Position;
+import com.vaadin.ui.Notification;
+
+public class u {
+
+		public static void uzen(String msg) {
+			// Notification with default settings for a warning
+			Notification notif = new Notification("Információ",msg,Notification.TYPE_WARNING_MESSAGE);
+			// Customize it
+			notif.setDelayMsec(400);
+			notif.setPosition(Position.BOTTOM_RIGHT);
+			notif.setStyleName("u_uzen");
+			//notif.setIcon(new ThemeResource("img/reindeer.png"));
+			// Show it in the page
+			notif.show(Page.getCurrent());
+		}
+	
+}
