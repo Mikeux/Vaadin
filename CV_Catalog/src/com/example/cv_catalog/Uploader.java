c8ab98a312345023490f690aac9b28f103425b95
Mikeux <mikeux@gmail.com>
2015-02-19 15:17:30 +0100

    UPLOAD PATH

--- CV_Catalog/src/com/example/cv_catalog/Uploader.java
+++ CV_Catalog/src/com/example/cv_catalog/Uploader.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 
 import com.vaadin.server.Page;
+import com.vaadin.server.VaadinService;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Upload.Receiver;
 import com.vaadin.ui.Upload.SucceededEvent;
@@ -14,20 +15,42 @@ public class Uploader implements Receiver, SucceededListener {
 	public File file;
 	
 	public OutputStream receiveUpload(String filename,String mimeType) {
-		// Create upload stream
+		
+		/*
+		https://vaadin.com/book/vaadin6/-/page/application.resources.html
+		https://vaadin.com/book/-/page/application.resources.html
+		
+		// Find the application directory
+		String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
+		
+		// Image as a file resource
+		FileResource resource = new FileResource(new File(basepath +
+		                        "/WEB-INF/images/image.png"));
+		
+		// Show the image in the application
+		Image image = new Image("Image from file", resource);
+		        
+		// Let the user view the file in browser or download it
+		Link link = new Link("Link to the image file", resource);
+		*/
+		
+		u.uzen("PATH => "+VaadinService.getCurrent().getBaseDirectory().getAbsolutePath());
+		
 		FileOutputStream fos = null; // Stream to write to
 		//File folder = new File ( System.getProperty( "user.home" ) + File.separator + "myfolder" ).mkdir();
 				
-	try {
-		file = new File("/" + filename);
-		fos = new FileOutputStream(file);
-	} catch (final java.io.FileNotFoundException e) {
-		new Notification("A fájl megnyitása nem sikerült<br/>",
-				e.getMessage(),
-				Notification.Type.ERROR_MESSAGE).show(Page.getCurrent());
 		return null;
-	}
-		return fos; // Return the output stream to write to
+		/*try {
+			file = new File("/" + filename);
+			fos = new FileOutputStream(file);
+		} catch (final java.io.FileNotFoundException e) {
+			new Notification("A fájl megnyitása nem sikerült<br/>",
+					e.getMessage(),
+					Notification.Type.ERROR_MESSAGE).show(Page.getCurrent());
+			return null;
+		}
+			return fos; // Return the output stream to write to
+		*/
 	}
 
 	@Override
