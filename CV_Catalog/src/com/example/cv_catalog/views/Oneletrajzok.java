4d0f64ff168e8977dd31765ebd9e358987dafd68
Mikeux <mikeux@gmail.com>
2015-02-20 06:51:55 +0100

    Filter

--- CV_Catalog/src/com/example/cv_catalog/views/Oneletrajzok.java
+++ CV_Catalog/src/com/example/cv_catalog/views/Oneletrajzok.java
@@ -1,25 +1,34 @@
 package com.example.cv_catalog.views;
 
+import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import javax.persistence.Query;
+
 import com.example.cv_catalog.u;
+import com.example.cv_catalog.model.Felhasznalok;
 import com.example.cv_catalog.model.KepzesSzint;
 import com.example.cv_catalog.model.Nyelvek;
 import com.example.cv_catalog.model.Oneletrajz;
 import com.example.cv_catalog.model.Orszagok;
+import com.example.cv_catalog.model.SzakmaiTapasztalat;
+import com.example.cv_catalog.model.SzemelyesAdatok;
 import com.vaadin.addon.jpacontainer.JPAContainer;
 import com.vaadin.addon.jpacontainer.JPAContainerFactory;
 import com.vaadin.addon.jpacontainer.fieldfactory.SingleSelectConverter;
 import com.vaadin.data.Container.Filter;
+import com.vaadin.data.Item;
 import com.vaadin.data.Property;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.fieldgroup.FieldGroup;
+import com.vaadin.data.util.IndexedContainer;
 import com.vaadin.data.util.converter.StringToDateConverter;
 import com.vaadin.data.util.filter.Compare;
+import com.vaadin.data.util.sqlcontainer.SQLContainer;
 import com.vaadin.data.validator.StringLengthValidator;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
@@ -47,17 +56,16 @@ public class Oneletrajzok extends VerticalLayout implements View {
 	private Button editButton = new Button("Szerkeszt");
 	private Button removeButton = new Button("Törlés");
 	private Button filterButton = new Button("Szûrés");
-	private ComboBox orszagokCombo,nyelvekCombo;
+	private Button defaultButton = new Button("Alaphelyzet");
+	private ComboBox orszagokCombo,nyelvekCombo,kepzesSzintCombo;
 	
 	private JPAContainer<Nyelvek> nyelvek;
 	private JPAContainer<Orszagok> orszagok;
-	private JPAContainer<Oneletrajz> oneletrajzok;
+	private JPAContainer<KepzesSzint> kepzesSzint;
+	private IndexedContainer oneletrajzok;
 	private Table oneletrajzokTable;
-	private FieldGroup editorFields = new FieldGroup();
 
-	private List<Filter> filters = new ArrayList<Filter>();
-	
-	private TextField id_field;
+	private TextField nev;
 	
 	public Oneletrajzok(UI ui){
 		mainLayout.setSizeFull();
@@ -71,63 +79,56 @@ public class Oneletrajzok extends VerticalLayout implements View {
 		
 		nyelvek = JPAContainerFactory.make(Nyelvek.class, "CV_Catalog");
 		orszagok = JPAContainerFactory.make(Orszagok.class, "CV_Catalog");
+		kepzesSzint = JPAContainerFactory.make(KepzesSzint.class, "CV_Catalog");
 		
-		//https://vaadin.com/forum#!/thread/1076201
-		oneletrajzok = JPAContainerFactory.make(Oneletrajz.class, "CV_Catalog");
-		oneletrajzok.addNestedContainerProperty("felhasznalok.nev");
-		oneletrajzok.addNestedContainerProperty("szemelyesAdatok.vezetekNev");
-		oneletrajzok.addNestedContainerProperty("szemelyesAdatok.keresztNev");
-		oneletrajzok.addNestedContainerProperty("szemelyesAdatok.szulIdo");		
+		this.adatotOsszeallit();
 		oneletrajzokTable = new Table("Önéletrajzok nyílvántartása", oneletrajzok);
-		oneletrajzokTable.addValueChangeListener(new Property.ValueChangeListener() {
-			public void valueChange(ValueChangeEvent event) {
-				Object id = oneletrajzokTable.getValue();
-				if (id != null)
-					editorFields.setItemDataSource(oneletrajzokTable.getItem(id));
-				//id_field.setReadOnly(true);
-			}
-		});
-		oneletrajzokTable.setVisibleColumns("felhasznalok.nev", "hozzaadva",
-												"szemelyesAdatok.vezetekNev",
-												"szemelyesAdatok.keresztNev",
-												"szemelyesAdatok.szulIdo");
-		oneletrajzokTable.setColumnHeader("felhasznalok.nev", "Létrehozta");
-		oneletrajzokTable.setColumnHeader("hozzaadva", "Hozzáadva");
-		oneletrajzokTable.setColumnHeader("szemelyesAdatok.vezetekNev", "Vezetéknév");
-		oneletrajzokTable.setColumnHeader("szemelyesAdatok.keresztNev", "Keresztnév");
-		oneletrajzokTable.setColumnHeader("szemelyesAdatok.szulIdo", "Születési idõ");
+		oneletrajzokTable.setVisibleColumns("letrehozta","vezeteknev","keresztnev","szulido");
+        oneletrajzokTable.setColumnHeader("letrehozta", "Hozzáadta");
+        oneletrajzokTable.setColumnHeader("vezeteknev", "Vezetéknév");
+        oneletrajzokTable.setColumnHeader("keresztnev", "Keresztnév");        
+        oneletrajzokTable.setColumnHeader("szulido", "Születési idõ");
 		
 		oneletrajzokTable.setSelectable(true);
 		oneletrajzokTable.setImmediate(true);	
 		tableLayout.addComponent(oneletrajzokTable);
 			
-		editLayout.addComponents(editButton,removeButton);
+		editLayout.addComponents(new HorizontalLayout(editButton,removeButton));
 		
 		Panel panel = new Panel("Szûrési feltételek");
 		Layout panelContent = new VerticalLayout();
-		//panel.setSizeUndefined();
-		//panelContent.setSizeUndefined();
+		panel.setSizeUndefined();
+		panelContent.setSizeUndefined();
 		
+		nev = new TextField("Név:");	
+		//v_nev.setColumns(20);
+		nev.setMaxLength(100);
+
 		orszagokCombo = new ComboBox("Származási ország:", orszagok);	
 		orszagokCombo.setContainerDataSource(orszagok);
 		orszagokCombo.setItemCaptionPropertyId("megnevezes");
 		orszagokCombo.setConverter(new SingleSelectConverter(orszagokCombo));
 		//editLayout.addComponents(orszagokCombo);
 				
-		nyelvekCombo = new ComboBox("Anyanyelv:", nyelvek);
+		nyelvekCombo = new ComboBox("Nyelvismeret:", nyelvek);
 		nyelvekCombo.setContainerDataSource(nyelvek);
 		nyelvekCombo.setItemCaptionPropertyId("nyelv");
 		nyelvekCombo.setConverter(new SingleSelectConverter(nyelvekCombo));
 		//editLayout.addComponents(nyelvekCombo);		
-		panelContent.addComponents(orszagokCombo,nyelvekCombo);
+		
+		kepzesSzintCombo = new ComboBox("Képesítés szintje:", kepzesSzint);	
+		kepzesSzintCombo.setContainerDataSource(kepzesSzint);
+		kepzesSzintCombo.setItemCaptionPropertyId("megnvezes");
+		kepzesSzintCombo.setConverter(new SingleSelectConverter(kepzesSzintCombo));		
+		
+		panelContent.addComponents(nev,orszagokCombo,nyelvekCombo,kepzesSzintCombo);
 		panel.setContent(panelContent);
 		
 		editLayout.addComponents(panel);	
 
-		editLayout.addComponents(filterButton);
-		
-		editorFields.setBuffered(false);
 
+		editLayout.addComponents(new HorizontalLayout(filterButton,defaultButton));
+		
         //setSizeFull();
 		//tableLayout.setSizeFull();
 		oneletrajzokTable.setSizeFull();
@@ -136,37 +137,100 @@ public class Oneletrajzok extends VerticalLayout implements View {
 		this.init();
 	}
 	
+	public void adatotOsszeallit(){
+		String where = "";
+		if(orszagokCombo != null) {
+			if(orszagokCombo.getValue() != null) where += "o2.id = "+orszagokCombo.getValue().toString();
+		}
+		
+		if(nyelvekCombo != null) {
+			if(nyelvekCombo.getValue() != null) {
+				if(where != "") where += " AND ";
+				where += "nye.id = "+nyelvekCombo.getValue().toString();
+			}
+		}		
+		
+		if(nev != null) {
+			if(nev.getValue() != null) {
+				if(where != "") where += " AND ";
+				where += "(sza.vezetekNev LIKE '%"+nev.getValue().toString()+"%' OR sza.keresztNev LIKE '%"+nev.getValue().toString()+"%')";
+			}
+		}	
+		
+		if(kepzesSzintCombo != null) {
+			if(kepzesSzintCombo.getValue() != null) {
+				if(where != "") where += " AND ";
+				where += "ksz.id = "+kepzesSzintCombo.getValue().toString();
+			}
+		}	
+				
+		List<Object[]> result = u.EM.createQuery("Select f,o,sza from Oneletrajz AS o "+
+				" JOIN Felhasznalok f ON f.oneletrajzok=o "+
+				" JOIN SzemelyesAdatok sza ON o.szemelyesAdatok=sza "+
+				" JOIN Orszagok o2 ON sza.orszagok=o2 "+
+				" JOIN Nyelvismeret nyi ON o.nyelvismeret=nyi "+
+				" JOIN Nyelvek nye ON nyi.nyelvek=nye "+
+				" JOIN Tanulmanyok ta ON o.tanulmanyok=ta "+
+				" JOIN KepzesSzint ksz ON ta.kepzesSzint=ksz "+
+				((where != "") ? " WHERE " + where :"")+
+				" GROUP BY o").getResultList();		
+		oneletrajzok  = new IndexedContainer();
+		oneletrajzok.addContainerProperty("letrehozta", String.class, "");
+		oneletrajzok.addContainerProperty("vezeteknev", String.class, "");
+		oneletrajzok.addContainerProperty("keresztnev", String.class, "");
+		oneletrajzok.addContainerProperty("szulido", Date.class, "");		
+		
+		for (Object[] row: result) {
+			Felhasznalok f = (Felhasznalok) row[0];
+			Oneletrajz or = (Oneletrajz) row[1];
+			SzemelyesAdatok sza = (SzemelyesAdatok) row[2];
+			Item newItem = oneletrajzok.getItem(oneletrajzok.addItem());
+			newItem.getItemProperty("letrehozta").setValue(f.getNev());
+			newItem.getItemProperty("vezeteknev").setValue(sza.getVezetekNev());
+			newItem.getItemProperty("keresztnev").setValue(sza.getKeresztNev());
+			newItem.getItemProperty("szulido").setValue(sza.getSzulIdo());
+		}		
+	}
+	
 	public void init() {
-		oneletrajzokTable.setConverter("szemelyesAdatok.szulIdo", new StringToDateConverter() {
+		oneletrajzokTable.setConverter("szulido", new StringToDateConverter() {
             @Override
             protected DateFormat getFormat(Locale locale) {
             	return new SimpleDateFormat("yyyy.MM.dd");
             }
         });		        
 		
-		oneletrajzokTable.setConverter("hozzaadva", new StringToDateConverter() {
-            @Override
-            protected DateFormat getFormat(Locale locale) {
-            	return new SimpleDateFormat("yyyy.MM.dd");
-            }
-        });				
 		
 		editButton.addClickListener(new ClickListener() {
 			public void buttonClick(ClickEvent event) {
 				Object Id = oneletrajzokTable.getValue();
-				getUI().getNavigator().navigateTo("cv_edit/"+Id);
+				if(Id != null) getUI().getNavigator().navigateTo("cv_edit/"+Id);
+				else u.uzen("Nincs kiválasztott elem!");
 			}
 		});
 		
 		
 		filterButton.addClickListener(new ClickListener() {
 			public void buttonClick(ClickEvent event) {
-				//Filter filter = new Compare.Equal("felhasznalok.nev", 1);
-				//filters.add(filter);
-				//oneletrajzok.addContainerFilter(filter);
+				adatotOsszeallit();
+				oneletrajzokTable.setContainerDataSource(oneletrajzok);
+				if(oneletrajzok.size()  > 0) oneletrajzokTable.select(oneletrajzok.getIdByIndex(0));	
+				u.uzen("Adatok leszûrve");
 			}
 		});
 		
+		defaultButton.addClickListener(new ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				nev.setValue("");
+				orszagokCombo.setValue(null);
+				nyelvekCombo.setValue(null);
+				kepzesSzintCombo.setValue(null);
+				adatotOsszeallit();
+				oneletrajzokTable.setContainerDataSource(oneletrajzok);
+				if(oneletrajzok.size()  > 0) oneletrajzokTable.select(oneletrajzok.getIdByIndex(0));	
+				u.uzen("Összes adat megjelenítése");
+			}
+		});		
 	}
 	
 	@Override
